46e162562758020992561e075591b9434b219a86
hoybonbon1233 <Kinghoop12345@gmail.com>
2026-01-30 17:15:39 +0700

    demo

--- calculator/calc.c
+++ calculator/calc.c
@@ -1,7 +1,37 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-
 int main() {
+    float a, b, result ;
+    char op ;
+    while(1) {
+    printf("Enter numbers and an operator (+, -, *, /, %%): ") ;
+    scanf("%f %c %f", &a, &op, &b) ;
+    
+    switch (op) {
+        case '+' :
+            result = a + b ;
+            break ;
+        case '-' :
+            result = a - b ;
+            break ;
+        case '*' :
+            result = a * b ;
+            break ; 
+        case '/' :
+            result = a / b;
+            break ;
+        case '%':
+            result = a % b ;
+            break ;
+        default :
+            printf("Error! Invalid operator.\n");
+            continue ;
+            
+    }
+    return 1 ;
+    }
 
+    printf("Result = %.2f\n", result) ;
+    return 0 ;
 }
