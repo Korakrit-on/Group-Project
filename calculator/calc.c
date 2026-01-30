c2df8a7da8a503a1880b46ba7b94e2fc833f2993
[captain] <captainza.055@gmail.com>
2026-01-30 19:39:06 +0700

    fix rid isDigit but add readFactor

--- calculator/calc.c
+++ calculator/calc.c
@@ -2,8 +2,21 @@
 #include <stdlib.h>
 
 // ฟังก์ชันตรวจสอบตัวเลข
-int isDigit(char c){
-    return(c >= '0' && c <= '9');
+double readFactor(char **p){
+    // เตรียมตัวชี้สำหรับการบอกจุดสิ้นสุดตัวเลข
+    char *end_ptr;
+
+    //เอาตำแหน่งหลังจากจบตัวเลข ไปใส่ไว้ใน end_ptr
+    double num = strtod(*p, &end_ptr);
+    // ตรวจสอบว่ามีการอ่านตัวเลขจริงไหม (ถ้า end_ptr ขยับ แปลว่าอ่านได้)
+    if (*p != end_ptr) {
+        // อัปเดตตำแหน่ง pointer หลักของเรา ให้ไปอยู่ที่เดียวกับ end_ptr
+        // (คือข้ามตัวเลขที่อ่านไปแล้ว)
+        *p = end_ptr;
+    }
+    
+    return num;
+
 }
 
 
